10db30958789c41a4e143bc99fe2dc25eaa75d10
Abdeljalil Elhassani <abdeljalilelhassani01@gmail.com>
2024-02-06 16:41:07 +0100

    adding github actions

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders university login header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/University Login/i);
+  expect(headerElement).toBeInTheDocument();
 });
